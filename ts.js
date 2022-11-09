f5b885fad3e4c9263f799413c91d2c759835f7a9
alfred776 <117074788+alfred776@users.noreply.github.com>
2022-11-09 17:20:03 +0100

    Add files via upload

---
+++ ts.js
@@ -0,0 +1,55 @@
+let person = {
+    name:'john' ,
+    cool: 10 ,
+}
+
+
+
+const musics = [
+    "wedjne - Annissa",
+    "keenV - Rien qu'une fois" ,
+    "M. pokora - A nos actes manqués" ,
+    "l'artiste - Mon pays",  
+]
+
+
+let trajets = {
+    change: 0,
+    radio: musics[0] ,
+    redlights: 30, 
+}
+
+function taketaxi(person, trajet) {
+    while(trajet.redlights > 0) {
+         
+    }
+   if(john) {
+    cool-1 
+   }
+
+
+  console.log('il perd 1 point')
+
+
+if(redlights=30){
+
+ console.log('il est arrivé à la maison')
+ 
+ console.log('explosion')
+}
+
+
+
+while(redlights=2) {
+   Math.ramdom(musics);
+   ramdom(-> wedjne)
+   return
+   person.cool -1
+   trajets.changement+1 
+}
+
+
+}
+
+
+
---
+++ tueur.js
@@ -0,0 +1,29 @@
+let nom= "jason";
+let point= 100;
+
+let array = ["nerd", "sportif", "blonde"];
+
+const names = [
+    'john',
+    "eduardo",
+    "eric",
+    "jeanne",
+    "elise",
+]
+
+let survivor1 = new survivor1(
+    names[math.floor (math.ramdom()=names.lenght)],
+    carac[Math.floor(math.ramdom() = carac.length)]
+) 
+
+console.log(survivor1)
+
+
+while(point>o,names=5) {
+  console.log()
+}
+
+
+
+
+
